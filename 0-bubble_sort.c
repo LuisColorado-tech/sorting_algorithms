a0257922ecd7aa2598c2dc6240208210e0d48fd1
LuisColorado-tech <luiscolorado@outlook.com>
2021-06-11 18:02:42 -0500

    Add files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,10 +13,16 @@ void bubble_sort(int *array, size_t size)
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
+                for(k = 0; k < size; k++)
+                {
+                    if (k < size - 1)
+                        printf("%d, ", array[k]);
+                    else
+                        printf("%d", array[k]);
+                }
+                printf("\n");
             }
-            for(k = 1; k < size; k++)
-                printf("%d, ", array[k]);
-            printf("\n");
         }
     }
 }
+
