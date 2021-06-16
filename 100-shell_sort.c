8d1800ee9834973c778ded626d248d9e16677918
LuisColorado-tech <luiscolorado@outlook.com>
2021-06-16 17:06:50 -0500

    Fix  and  bigo o notation

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,34 +1,34 @@
 #include "sort.h"
+
 /**
- * shell_sort - Shell Sort Algorithm
- * @array: Array to be sorted
- * @size: Size of the Array
- * Return: Void
- */
+* shell_sort - Sorts array using Knuth Sequence of Shell sort
+* @array: Array to be sorted
+* @size: Size of array to sort
+*/
+
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1;
-	unsigned int i, j;
-	int temp;
+	size_t i, gap = 1, k;
+	int v;
 
-	if (array == NULL || size < 2)
+	if (array == NULL)
 		return;
-
-	while (gap < size / 3)
+	while (gap < size)
+		gap = 3 * gap + 1;
+	if (gap >= size)
+		gap = (gap - 1) / 3;
+	while (gap > 0)
 	{
-		gap = gap * 3 + 1;
-	}
-	for (; gap >= 1; gap /= 3)
-	{
-		for (i = gap; i < size; i += 1)
+		for (i = gap; i < size; i++)
 		{
-			temp = array[i];
-			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			v = array[i];
+			for (k = i; (k > gap - 1) && (v < array[k - gap]); k -= gap)
 			{
-				array[j] = array[j - gap];
+				array[k] = array[k - gap];
 			}
-			array[j] = temp;
+			array[k] = v;
 		}
+		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
 }
