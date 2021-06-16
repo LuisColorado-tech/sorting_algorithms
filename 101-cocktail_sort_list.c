425becc9cf759c8ba59e0216891269488ce1ae8e
LuisColorado-tech <luiscolorado@outlook.com>
2021-06-16 17:02:02 -0500

    Add file

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * cocktail_sort_list - Selection Sort Algorithm
+ * @array: Array to be sorted
+ * @size: Size of the Array
+ * Return: Void
+ */
+void cocktail_sort_list(listint_t **list)
+{
+    
+}
